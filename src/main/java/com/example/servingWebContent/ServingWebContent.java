8b09414a2ad3cc3fcd3cd588d9bedf4077a77cfe
sorokaa <sasha20999@outlook.com>
2020-12-14 15:46:39 +0200

    Try to set Spring Security

--- src/main/java/com/example/servingWebContent/ServingWebContent.java
+++ src/main/java/com/example/servingWebContent/ServingWebContent.java
@@ -2,11 +2,16 @@ package com.example.servingWebContent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-//@EnableWebMvc
+@EnableWebMvc
+@EntityScan("com.example.domain")
+@EnableJpaRepositories("com.example.repository")
 public class ServingWebContent extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
@@ -16,7 +21,8 @@ public class ServingWebContent extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         if (!registry.hasMappingForPattern("/assets/**")) {
-            registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+            registry.addResourceHandler("/assets/**")
+                    .addResourceLocations("classpath:/assets/");
         }
     }
 }
